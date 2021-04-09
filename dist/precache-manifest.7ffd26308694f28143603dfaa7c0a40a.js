d074ad501a370ffc57f2460887706840bee7aada
Mike Conrad <mike@enxo.co>
2021-04-09 09:50:27 -0400

    Added updated production build

--- dist/precache-manifest.7ffd26308694f28143603dfaa7c0a40a.js
+++ dist/precache-manifest.7ffd26308694f28143603dfaa7c0a40a.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5c137bd6413587f210f1",
-    "url": "/time-tracker/dist/css/app.4b63299c.css"
+    "revision": "32d7b6c0c239a01d741e",
+    "url": "/time-tracker/dist/css/app.af81d8fa.css"
   },
   {
     "revision": "fa2a5959757034c9aa1fd13c8cf65a68",
@@ -10,6 +10,22 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "1a8a23e546a134536cd0b0c580c25602",
     "url": "/time-tracker/dist/img/deleteIcon.png"
+  },
+  {
+    "revision": "efcb9035d52df7487aa23e6785d1156a",
+    "url": "/time-tracker/dist/img/fi-rr-cross-small.svg"
+  },
+  {
+    "revision": "a26c3751652a6980f3b54f8621b547a9",
+    "url": "/time-tracker/dist/img/fi-rr-disk.svg"
+  },
+  {
+    "revision": "547f3b23c5da21f47c2200aff88858eb",
+    "url": "/time-tracker/dist/img/fi-rr-play.svg"
+  },
+  {
+    "revision": "f03555ee1fcc146737227cc356156dec",
+    "url": "/time-tracker/dist/img/fi-rr-stop.svg"
   },
   {
     "revision": "c31ca587f406d4bb289497c67031fdb9",
@@ -28,16 +44,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/time-tracker/dist/img/stopIcon.png"
   },
   {
-    "revision": "34ced65cdcbf219cc2bf0187a6d3eee7",
+    "revision": "ad1cfc4d1c2e4af675e806aa370569e4",
     "url": "/time-tracker/dist/index.html"
   },
   {
-    "revision": "5c137bd6413587f210f1",
-    "url": "/time-tracker/dist/js/app.5b9f0e20.js"
+    "revision": "32d7b6c0c239a01d741e",
+    "url": "/time-tracker/dist/js/app.223e551c.js"
   },
   {
-    "revision": "8886a7a0437e4a65347d",
-    "url": "/time-tracker/dist/js/chunk-vendors.f4000ae0.js"
+    "revision": "848825461844b6c1cd05",
+    "url": "/time-tracker/dist/js/chunk-vendors.0bf4f709.js"
   },
   {
     "revision": "f89eae5c3c4e7439a9401f6cbf9c5e32",
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/time-tracker/dist/precache-manifest.3fcb6716d56a03fafb11ea559b6b737a.js"
+  "/time-tracker/dist/precache-manifest.7ffd26308694f28143603dfaa7c0a40a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "todo-app"});
